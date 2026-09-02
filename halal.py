b42c081fb634de7d6dcce307d9ab6aa8b256f6b0
Tusnane <uklonskiianatoliI@gmail.com>
2026-09-02 09:51:13 +0700

    обновлёный сикс севен

--- halal.py
+++ halal.py
@@ -1,2 +1,9 @@
-for i in range(67):
-    print('HALAL')
+import random
+cnt=0
+hui=random.randint(1,67)
+for i in range(hui):
+    for i in range(hui):
+        print('сик севен')
+        cnt+=1
+print(cnt,'коллво сикс севен')        
+print('рандомный сикс севен напечатан')
